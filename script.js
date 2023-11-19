0b24c25a61ce07456aec9077eca9819db5b62a33
El-Duende <131217632+El-Duende@users.noreply.github.com>
2023-11-18 21:11:04 -0500

    Added error checking on Full name field

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     let firstName = document.getElementById("firstName");
     let nameValid = document.getElementById("nameValid");
- 
-
+    let nameValidBool = false;
     firstName.addEventListener('input', validateInput);
 
+    let SSN = document.getElementById("SSN");
+    let SSNValid = document.getElementById("SSNVaild");
+    SSN.addEventListener('input', validateRegX("\d\d\d-\d\d-\d\d\d\d", SSNValid));
+    
+
+  
 
     function validateInput(e){
-        if(e.target.value.length > 0){
+        if(e.target.value.length > 5){
             nameValid.classList.add('was-validated');
+            nameValid.classList.remove('is-incorrect');
+
+            nameValidBool = true;
         }
-        console.log("Testing");
+        else if(e.target.value.length < 5 && nameValidBool == true){
+            nameValid.classList.remove('was-validated');
+            nameValid.classList.add('is-incorrect');
+            
+          
+            
+            nameValidBool = false;
+            console.log("less than 5");
+        }
+    }
+
+    function validateRegX(e, String, HTMLElement){
+        regX = String;
+        console.log(regX);
     }
 });
