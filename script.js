e8f54eca218871ae83545f43f8fff9abe520ba3b
El-Duende <131217632+El-Duende@users.noreply.github.com>
2023-11-19 01:06:48 -0500

    validates most of address now

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const ourForm = document.querySelector('#ourForm');
-    const regSSN = /\d\d\d-\d\d-\d\d\d\d/;
+    const regSSN = /^\d\d\d-\d\d-\d\d\d\d$/;
+    const regPhone = /^\d\d\d-\d\d\d-\d\d\d\d$/;
+    const regEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    
 
     let firstName = document.getElementById("firstName");
     var nameValidBool = false;
@@ -13,16 +16,45 @@ document.addEventListener("DOMContentLoaded", function () {
     let SSNValid = document.getElementById("SSNValid");
     SSN.addEventListener('input',function(e){ validateReg(e,"SSNValidBool",SSNValid, regSSN)});
 
-  
-    // listen for keyup and change events
+    let phoneNumber = document.getElementById("phoneNumber");
+    var phoneValidBool = false;
+    let phoneValid = document.getElementById("phoneValid");
+    phoneNumber.addEventListener('input',function(e){ validateReg(e,"phoneValidBool",phoneValid, regPhone)});
+
+    let altPhoneNumber = document.getElementById("altPhoneNumber");
+    var altphoneValidBool = false;
+    let altPhoneValid = document.getElementById("altPhoneValid");
+    altPhoneNumber.addEventListener('input',function(e){ validateReg(e,"altphoneValidBool",altPhoneValid, regPhone)});
+
+    let email = document.getElementById("email");
+    var emailValidBool = false;
+    let emailValid = document.getElementById("emailValid");
+    email.addEventListener('input',function(e){ validateReg(e,"emailValidBool",emailValid, regEmail)});
+
+    let streetAddress = document.getElementById("streetAddress");
+    var streetAddressValidBool = false;
+    let streetAddressValid = document.getElementById("streetAddressValid");
+    streetAddress.addEventListener('input',function(e){ validateInput(e,"streetAddressValidBool",streetAddressValid)});
+
+
+    let cityAddress = document.getElementById("cityAddress");
+    var cityAddressValidBool = false;
+    let cityAddressValid = document.getElementById("cityAddressValid");
+    cityAddress.addEventListener('input',function(e){ validateInput(e,"cityAddressValidBool",cityAddressValid)});
+
+    let stateAddress = document.getElementById("stateAddress");
+    var stateAddressVaildBool = false;
+    let stateAddressValid = document.getElementById("stateAddressValid");
+    stateAddress.addEventListener('change',function(e){ validateState(e,"stateAddressVaildBool",stateAddressValid)});
+
+    // Listens to changes and updates the progress bar at the top.
     ['keyup', 'change'].forEach((type) => {
       // add the listeners to the form
       ourForm.addEventListener(type, (event) => {
         //check if each section is good to go here. 
        console.log("change to form");
-       checkGeneralInfo("nameValidBool","SSNValidBool");
-       console.log(window["nameValidBool"]);
-
+       checkGeneralInfo("nameValidBool","SSNValidBool","phoneValidBool","altphoneValidBool","emailValidBool",
+       "streetAddressValidBool","cityAddressValidBool","stateAddressVaildBool");
       })
     })
     
@@ -34,13 +66,33 @@ document.addEventListener("DOMContentLoaded", function () {
             inputVaild.classList.remove('is-incorrect');
 
             window[valueBool] = true;
+            console.log("its longer than 2");
         }
         else if(e.target.value.length == 1){
             inputVaild.classList.remove('was-validated');
             inputVaild.classList.add('is-incorrect');
             window[valueBool] = false;
         }
+        console.log("Its trying too.");
     }
+
+    function validateState(e,valueBool,inputVaild){
+        console.log(window[valueBool]);
+        if(e.target.value != "State"){
+            inputVaild.classList.add('was-validated');
+            inputVaild.classList.remove('is-incorrect');
+
+            window[valueBool] = true;
+            console.log("its longer than 2");
+        }
+        else{
+            inputVaild.classList.remove('was-validated');
+            inputVaild.classList.add('is-incorrect');
+            window[valueBool] = false;
+        }
+        console.log("Its trying too.");
+    }
+
 
     function validateReg(e,valueBool,inputVaild, regX){
         let reg = regX
@@ -58,11 +110,13 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function checkGeneralInfo(nameValidBool,SSNValidBool){
+    function checkGeneralInfo(nameValidBool,SSNValidBool,phoneValidBool,altphoneValidBool,emailValidBool,streetAddressValidBool,
+        cityAddressValidBool,stateAddressVaildBool){
         let general = document.getElementById("generalNav");
         let submitbttn = document.getElementById("submitFormBtn");
 
-        if(window[nameValidBool] & window[SSNValidBool]){
+        if(window[nameValidBool] & window[SSNValidBool] & window[phoneValidBool] & window[altphoneValidBool]
+            &window[emailValidBool] & window[streetAddressValidBool] & window[cityAddressValidBool] & window[stateAddressVaildBool]){
           
            general.classList.remove('incomplete');
            general.classList.add('complete');
